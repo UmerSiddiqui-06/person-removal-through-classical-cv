fb1d2c9a74ffb7205c3a99570e4f8a573e099145
Umer Siddiqui <bsai23038@itu.edu.pk>
2025-09-13 17:35:51 +0500

    ploting function added

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 import os
 import numpy as np
 from PIL import Image
-
-def rgb_to_gray(img: np.ndarray) -> np.ndarray:
+import matplotlib.pyplot as plt
+def _rgb_to_gray(img: np.ndarray) -> np.ndarray:
     # extract R, G, B
     R = img[:, :, 0]
     G = img[:, :, 1]
@@ -27,8 +27,6 @@ def read_frames(input_folder: str) -> np.ndarray:
         F: total no of frames, H: Height, W: Width, C: Channels
         or [F, H, W] for grayscale
 
-    # write your code here
-    # ...
     """
     frames = []
     file_list = sorted(os.listdir(input_folder))
@@ -38,13 +36,43 @@ def read_frames(input_folder: str) -> np.ndarray:
             path = os.path.join(input_folder, filename)
             img = Image.open(path)
             img = np.array(img)  # now [H, W, 3]
-            gray = rgb_to_gray(img)  # manual conversion
+            gray = _rgb_to_gray(img)  # manual conversion
             frames.append(gray)
 
     frames = np.stack(frames, axis=0)  # [F, H, W]
     return frames
 
+
+
+def plot_frames(frames: np.ndarray, num_frames: int, save_name: str) -> None:
+    """
+    Plots and saves multiple frames in a single image.
+
+    Input:
+        frames (list): List of frame arrays
+        num_frames (int): Number of frames to display
+        save_name (str): Name for the saved image file
+    """
+     # select frames to plot
+    selected_frames = frames[:num_frames]
+
+    # make grid: rows x cols
+    cols = 5
+    rows = (num_frames + cols - 1) // cols  # ceil division
+
+    plt.figure(figsize=(15, 3 * rows))
+
+    for i, frame in enumerate(selected_frames):
+        plt.subplot(rows, cols, i + 1)
+        plt.imshow(frame, cmap="gray")
+        plt.axis("off")
+        plt.title(f"Frame {i}")
+
+    plt.tight_layout()
+    plt.savefig(save_name, dpi=300, bbox_inches="tight")
+    plt.close()
 # Example Usage:
 input_folder = "input/snowFall_frames"
 frames = read_frames(input_folder)
-print(f"Frames shape: {frames.shape}")  # [F, H, W]
+print(f"Frames shape: {frames.shape}")  # [F, H, W] 
+plot_frames(frames, num_frames=10, save_name="report.pdf")
